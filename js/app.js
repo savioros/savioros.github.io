5508d0cdf77ac82493f353b871a9a02e97260fdc
Sávio Rian <saviopeck@outlook.com>
2022-06-08 21:38:20 -0300

    Efeito de opacidade ao carregar a página

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-const links = document.querySelectorAll('.menu a')
-
-function redirecionar(event){
-    event.preventDefault()
+function scrollSuave(){
+    const links = document.querySelectorAll('.menu a')
     
-    const href = this.getAttribute('href')
-    const section = document.querySelector(href)
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+    function redirecionar(event){
+        event.preventDefault()
+        
+        const href = this.getAttribute('href')
+        const section = document.querySelector(href)
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+    
+    function eventosLinks(link){
+        link.addEventListener("click", redirecionar)
+    }
+    
+    links.forEach(eventosLinks)
+}
+scrollSuave()
+
+function animaAoCarregar(){
+    
+    const sections = document.querySelectorAll('.js-animacao')
+    sections.forEach((section) => {
+        section.classList.add('aparece-ao-carregar')
     })
-}
 
-function eventosLinks(link){
-    link.addEventListener("click", redirecionar)
 }
-
-links.forEach(eventosLinks)
+window.addEventListener('load', animaAoCarregar)
